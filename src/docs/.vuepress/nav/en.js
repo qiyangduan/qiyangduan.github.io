42b6841370d8ffea4ad98812ff9c4e593db2c0d1
qiyangduan <qiyang.duan@alibaba-inc.com>
2021-06-07 23:38:30 +0100

    removing dispatch_doc repo

---
+++ src/docs/.vuepress/nav/en.js
@@ -0,0 +1,31 @@
+module.exports = [
+  {
+    text: 'Quickstart & Tutorial',
+    link: '/doc/'
+  },
+  {
+    text: 'Document',
+    items: [
+      {
+        text: 'Installation',
+        link: '/miscellaneous/local-development.html'
+      },
+      {
+        text: 'Concepts',
+        link: '/miscellaneous/design-concepts.html'
+      },
+      {
+        text: 'API Spec', 
+        link: 'https://londondemo1.dispatch.kandbox.com/api/v1/docs/'
+      },
+      {
+        text: 'FAQ',
+        link: '/faq/'
+      },
+      {
+        text: 'Glossary',
+        link: '/miscellaneous/glossary.html'
+      }
+    ]
+  }
+]
